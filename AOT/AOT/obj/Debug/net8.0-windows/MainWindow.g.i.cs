70b51af7e744a19416636197078c77522c263739
Paramie <paramie.willmann3@gmail.com>
2025-05-12 00:15:33 +0200

    update

--- AOT/AOT/obj/Debug/net8.0-windows/MainWindow.g.i.cs
+++ AOT/AOT/obj/Debug/net8.0-windows/MainWindow.g.i.cs
@@ -61,7 +61,7 @@ namespace AOT {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AOT;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AOT;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
