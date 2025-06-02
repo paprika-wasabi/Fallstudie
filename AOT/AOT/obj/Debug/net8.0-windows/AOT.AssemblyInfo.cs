c373022de06d1b41e9073653a48cddea20d9c710
DaniVot <144905638+DaniVot@users.noreply.github.com>
2025-06-02 19:42:10 +0200

    UI Update

--- AOT/AOT/obj/Debug/net8.0-windows/AOT.AssemblyInfo.cs
+++ AOT/AOT/obj/Debug/net8.0-windows/AOT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AOT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d726b1c0afba3558a9667e8fe51f21af80062d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7050806d3bdda490a51988daf6dd2e034cc6c8fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AOT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AOT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
