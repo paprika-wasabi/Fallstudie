8f5397d17221369a1f308f0e91337e24b3c448e3
DaniVot <144905638+DaniVot@users.noreply.github.com>
2025-06-03 22:38:04 +0200

    Logo Updated

--- AOT/AOT/obj/Debug/net8.0-windows/AOT.AssemblyInfo.cs
+++ AOT/AOT/obj/Debug/net8.0-windows/AOT.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AOT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63ddf9c87e430ed4e95e9880fe47d1bfe654ee7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1893f1c81953adad7c23657cb0477aaa23b28fed")]
 [assembly: System.Reflection.AssemblyProductAttribute("AOT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AOT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
